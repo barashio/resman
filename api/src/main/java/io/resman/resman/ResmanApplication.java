2eef67d46adf021b46c9cacdbfa033457c5f0ed9
barashio <barashhyusein@yahoo.com>
2021-03-29 04:41:16 +0100

    commit

--- api/src/main/java/io/resman/resman/ResmanApplication.java
+++ api/src/main/java/io/resman/resman/ResmanApplication.java
@@ -2,8 +2,6 @@ package io.resman.resman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 public class ResmanApplication {
